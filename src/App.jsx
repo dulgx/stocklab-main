d1133f5056e21611b4fbb7ccda7d9ec4cc2c6cce
dulgx <dulguunnu@gmail.com>
2024-03-06 18:19:10 +0800

    #68 Вебийн мастер хуудас

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import './styles/styles.scss';
 import Navigation from './pages/Navigation';
+import HeroComponent from './pages/HeroComponent';
 
 function App() {
 
   return (
     <div className='App'>
       <Navigation />
+      <HeroComponent />
     </div>
   )
 }
---
+++ src/pages/HeroComponent.jsx
@@ -0,0 +1,8 @@
+const HeroComponent = () => {
+    return (
+        <section className='hero-section'>
+            <p>Page</p>
+        </section>
+    )
+}
+export default HeroComponent;

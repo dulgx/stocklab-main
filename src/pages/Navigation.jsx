080841567df8f5b2e5b17b9f2686ddba42e09dcb
dulgx <dulguunnu@gmail.com>
2024-04-15 18:30:24 +0800

    tailwind added

--- src/pages/Navigation.jsx
+++ src/pages/Navigation.jsx
@@ -1,54 +1,66 @@
-import stocklabLogo from '../assets/stocklab-logo.png';
-import facebookLogo from '../assets/logo-fb-simple-2.png';
-import instaLogo from '../assets/logo-instagram-2.png';
-import down from '../assets/vector-1.svg';
+import stocklabLogo from "../assets/stocklab-logo.png";
+import facebookLogo from "../assets/logo-fb-simple-2.png";
+import instaLogo from "../assets/logo-instagram-2.png";
+import down from "../assets/vector-1.svg";
 
-import { Link, NavLink } from 'react-router-dom';
-import { useState } from 'react';
+import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
 
 const Navigation = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
 
-    const [showNavbar, setShowNavbar] = useState(false);
+  const navbarShow = () => {
+    setShowNavbar(!showNavbar);
+  };
 
-    const navbarShow = () => {
-        setShowNavbar(!showNavbar);
-    }
+  return (
+    <nav>
+      <div className="nav-elements flex flex-row m-5">
+        <div>
+          <NavLink to="/">
+            {" "}
+            <img id="StocklabLogo" src={stocklabLogo} alt="stocklab-logo" />
+          </NavLink>
+        </div>
 
-    return (
-        <nav>
-            <div className={`nav-elements  ${showNavbar && "active"}`}>
-                <div>
-                    <NavLink to="/"> <img id='StocklabLogo' src={stocklabLogo} alt="stocklab-logo" /></NavLink>
-                </div>
+        <div>
+          <ul className="nav-link-container">
+            <li>
+              <NavLink to="aboutus">Бидний тухай</NavLink>
+            </li>
+            <li>
+              <NavLink to="/services">
+                Үйлчилгээ <img src={down} alt="down" id="DownVector" />
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/news">Мэдээлэл</NavLink>
+            </li>
+            <li>
+              <NavLink to="/instructions">Заавар</NavLink>
+            </li>
+          </ul>
 
-                <div>
-                    <ul className='nav-link-container'>
-                        <li>
-                            <NavLink to="aboutus">Бидний тухай</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/services">Үйлчилгээ <img src={down} alt="down" id='DownVector' /></NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/news">Мэдээлэл</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="/instructions">Заавар</NavLink>
-                        </li>
-                    </ul>
-
-                    <div className='social-links'>
-                        <a href="#">
-                            <img className='social-logo' src={facebookLogo} alt="facebook-logo" />
-                        </a>
-                        <a href="#">
-                            <img className='social-logo' src={instaLogo} alt="instagram-logo" />
-                        </a>
-                    </div>
-                    <button id='LoginButton'>Нэвтрэх</button>
-                </div>
-            </div>
-        </nav>
-    )
-}
-export default Navigation
+          <div className="social-links">
+            <a href="#">
+              <img
+                className="social-logo"
+                src={facebookLogo}
+                alt="facebook-logo"
+              />
+            </a>
+            <a href="#">
+              <img
+                className="social-logo"
+                src={instaLogo}
+                alt="instagram-logo"
+              />
+            </a>
+          </div>
+          <button id="LoginButton">Нэвтрэх</button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navigation;
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,20 +1,15 @@
-import leftIcon from './assets/left-icon.png'
-import rightIcon from './assets/right-icon.png'
-import PageOne from './page-1'
-import '../../styles/service.scss'
+import leftIcon from "./assets/left-icon.png";
+import rightIcon from "./assets/right-icon.png";
+import PageOne from "./page-1";
 
 const Services = () => {
-    
-    return(
-        <div className="service-container">
-            <img src={leftIcon} alt="left icon" className='left-right-icon' />
-            <PageOne />
-            <img src={rightIcon} alt="right icon" className='left-right-icon' />
-        </div>
-    )
-}
+  return (
+    <div className="flex items-center justify-center">
+      <img src={leftIcon} alt="left icon" className="left-right-icon" />
+      <PageOne />
+      <img src={rightIcon} alt="right icon" className="left-right-icon" />
+    </div>
+  );
+};
 
-export default Services   
-
-
-
+export default Services;
